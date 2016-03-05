0151af9ad3bcf78371985a48f8ef4423f4530a85
charlieperson <charlie.person@hyperisland.se>
2016-03-05 19:19:30 +0000

    Switching from sqlite3 to postgresql for Heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160304144003) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text     "content"
     t.integer  "question_id"
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 20160304144003) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
